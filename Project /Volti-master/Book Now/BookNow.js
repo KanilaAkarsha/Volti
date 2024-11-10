0efd442f1d07179afa9607609b1299f055c9a8e9
KanilaAkarsha <Kanilasaman1@gmail.com>
2024-11-10 20:53:08 +0530

    commit

--- Project /Volti-master/Book Now/BookNow.js
+++
@@ -1,5 +0,0 @@
-// Add event listener for form submission
-document.getElementById("bookingForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent default form submission
-    alert("Thank you! Your charging slot has been booked.");
-  });
